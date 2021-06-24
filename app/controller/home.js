ba7272d43cf7949f518407506f456e9401df21b6
sy <7525117+huolingmengling@user.noreply.gitee.com>
2021-06-24 15:21:09 +0800

    no message

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
-const Controller = require("egg").Controller;
+const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    const { ctx } = this;
-    const User = this.app.model.User;
-    await new User({
-      userName: "loz",
-      password: "123",
-    }).save();
-    ctx.body = "hi, egg";
-  }
+	async index() {
+		const { ctx } = this;
+		const User = this.app.model.User;
+		await new User({
+			userName: 'loz',
+			password: '123',
+		}).save();
+		ctx.body = '你好, egg';
+	}
 }
 
 module.exports = HomeController;
--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ module.exports = app => {
 	const auth = app.middleware.auth();
 
 	router.prefix('/api/v1'); // 设置基础路径
+	router.get('/', controller.home.index);
 	router.post('/users', controller.user.create);
 	router.post('/users/login', controller.user.login);
 	router.get('/user', auth, controller.user.getCurrentUser);
